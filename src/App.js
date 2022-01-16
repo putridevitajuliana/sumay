03210120a40a109eda9f24f5ec8c1d8accb6583d
putridevitajuliana <97815949+putridevitajuliana@users.noreply.github.com>
2022-01-16 15:00:57 +0700

    sumay
    sumay

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "/App.ces";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">header</div>
+
+      <div className="section">
+        <div className="card section-1">1</div>
+        <div className="card section-2">2</div>
+        <div className="card section-3">3</div>
+        <div className="card section-4">4</div>
+        <div className="card section-5">5</div>
+        <div className="card section-6">6</div>
+      </div>
+
+      <div className="body">
+        <div className="body-anak body-1">1</div>
+        <div className="body-anak body-2">2</div>
+        <div className="body-anak body-3">3</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
